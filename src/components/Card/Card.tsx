df60a44921cd45cd7a687d463bd74bf6b4550767
Willian <williansilvap25@gmail.com>
2023-09-11 23:49:48 -0300

    add a input type range to take value of vote

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
 import { IMovie } from "../../types/MoviesType";
 
 export const Card = ({ poster_path, title, vote_average }: IMovie) => {
+  const [vote, setVote] = useState(0);
+
+  const handleSubmit = (ev: React.SyntheticEvent) => {
+    ev.preventDefault();
+    console.log(vote)
+  };
+
   return (
     <div>
       <img src={`${import.meta.env.VITE_IMG_MD}${poster_path}`} alt={title} />
       <h3>{title}</h3>
       <span>{vote_average}</span>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="vote">Avaliar</label>
+        <input
+          id="vote"
+          type="range"
+          min={0}
+          max={10}
+          step={0.1}
+          value={vote}
+          onChange={(ev) => setVote(+ev.target.value)}
+        />
+        <span>{vote}</span>
+        <button>Enviar</button>
+      </form>
     </div>
   );
 };
