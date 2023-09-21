647607792a2fe11d48115ac158807546c92b0fb2
Willian <williansilvap25@gmail.com>
2023-09-20 22:14:01 -0300

    add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <>
+      <p>footer</p>
+    </>
+  );
+};
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
 
 import styles from "./Layout.module.scss";
 
@@ -13,7 +14,9 @@ export const Layout = () => {
       <main className={styles.main}>
         <Outlet />
       </main>
-      <footer className={styles.footer}>FOOTER</footer>
+      <footer className={styles.footer}>
+        <Footer />
+      </footer>
     </>
   );
 };
