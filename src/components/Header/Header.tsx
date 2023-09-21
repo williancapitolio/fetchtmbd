f563b1b99ca0beecf49ee57dcad7cafa256a13a9
Willian <williansilvap25@gmail.com>
2023-09-20 23:06:25 -0300

    header styles ok

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import styles from "./Header.module.scss";
 
 export const Header = () => {
+  const [active, setActive] = useState({
+    home: "",
+    rated: "inactiveLink",
+  });
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActive({
+        home: "",
+        rated: "inactiveLink",
+      });
+    } else if (location.pathname === "/rated-movies") {
+      setActive({
+        home: "inactiveLink",
+        rated: "",
+      });
+    }
+  }, [location]);
+
   return (
-    <div>
-      <Link to={"/"}>FetchTMDB</Link>
-      <span>Olá visitante!</span>
-      <Link to={"rated-movies"}>Filmes Avaliados</Link>
+    <div className={styles.wrapper}>
+      <Link to={"/"} className={styles.wrapperLogo}>
+        FetchTMDB
+      </Link>
+      <div className={styles.wrapperLinks}>
+        <Link to={"/"} className={`${styles.wrapperLinksLink} ${active.home}`}>
+          Início
+        </Link>
+        <Link
+          to={"rated-movies"}
+          className={`${styles.wrapperLinksLink} ${active.rated}`}
+        >
+          Avaliações
+        </Link>
+      </div>
+      <span className={styles.wrapperUser}>Olá Visitante!</span>
     </div>
   );
 };
