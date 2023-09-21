a1d13c1f0da9705bf858cae783927f58bd9de07c
Willian <williansilvap25@gmail.com>
2023-09-20 22:26:48 -0300

    add footer and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,14 @@
+import styles from "./Footer.module.scss";
+
 export const Footer = () => {
   return (
-    <>
-      <p>footer</p>
-    </>
+    <div className={styles.wrapper}>
+      <a href="https://github.com/williancapitolio/fetchtmbd" target="_blank" className={styles.wrapperLink}>
+        FetchTMDB
+      </a>
+      <a href="https://github.com/williancapitolio" target="_blank" className={styles.wrapperLink}>
+        GitHub
+      </a>
+    </div>
   );
 };
