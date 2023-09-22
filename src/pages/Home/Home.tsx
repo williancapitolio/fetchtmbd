9035b8b29915e58f760db6caf5b559f886f01048
Willian <williansilvap25@gmail.com>
2023-09-21 22:04:43 -0300

    start style home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,25 +4,29 @@ import { Card } from "../../components/Card";
 
 import { MoviesType } from "../../types/MoviesType";
 
+import styles from "./Home.module.scss"
+
 export const Home = () => {
   const { movies } = useLoaderData() as MoviesType;
 
   return (
-    <>
-      <h1>Filmes em Cartaz</h1>
-      {movies.results
-        .sort((a, b) => b.vote_average - a.vote_average)
-        .map(({ id, poster_path, rating, title, vote_average }) => (
-          <Card
-            key={id}
-            id={id}
-            poster_path={poster_path}
-            rating={rating}
-            title={title}
-            vote_average={vote_average}
-            ratingAction="add"
-          />
-        ))}
-    </>
+    <section className={styles.wrapper}>
+      <h1 className={styles.wrapperTitle}>Filmes em Cartaz</h1>
+      <section className={styles.wrapperMovies}>
+        {movies.results
+          .sort((a, b) => b.vote_average - a.vote_average)
+          .map(({ id, poster_path, rating, title, vote_average }) => (
+            <Card
+              key={id}
+              id={id}
+              poster_path={poster_path}
+              rating={rating}
+              title={title}
+              vote_average={vote_average}
+              ratingAction="add"
+            />
+          ))}
+      </section>
+    </section>
   );
 };
