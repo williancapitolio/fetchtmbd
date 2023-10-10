7a24b89e0c46d85a0ce74447088933dffb4426a8
Willian <williansilvap25@gmail.com>
2023-10-09 22:20:12 -0300

    update url env

--- src/loaders/searchedMoviesLoader.ts
+++ src/loaders/searchedMoviesLoader.ts
@@ -7,7 +7,7 @@ export const searchedMoviesLoader = async ({
 }: LoaderFunctionArgs): Promise<MoviesType> => {
   if (!params) throw new Error("Something went wrong...");
 
-  const url = `https://api.themoviedb.org/3/search/movie?language=pt-BR&api_key=981b9f0c15739d4fff4d829853f4dce8&query=${params.query}`;
+  const url = `${import.meta.env.VITE_URL_SEARCHED_MOVIES}${params.query}`;
   const options = { method: "GET", headers: { accept: "application/json" } };
 
   const results = await fetch(url, options);
