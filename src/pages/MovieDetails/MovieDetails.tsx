1246742777425517dae90b639882c5c053ae8179
Willian <williansilvap25@gmail.com>
2023-10-13 16:27:54 -0300

    add a style bagde for vote on details movie page

--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -8,14 +8,27 @@ export const MovieDetails = () => {
   const { poster_path, title, vote_average, backdrop_path, overview, runtime } =
     useLoaderData() as IMovie;
 
+  const verifyVoteColor = (vote_average: number): string => {
+    let voteColor: string = "";
+
+    if (vote_average <= 2) voteColor += "#ff4545";
+    else if (vote_average <= 4) voteColor += "#ffa534";
+    else if (vote_average <= 6) voteColor += "#ffe234";
+    else if (vote_average <= 8) voteColor += "#b7dd29";
+    else if (vote_average > 8) voteColor += "#57e32c";
+    else if (!vote_average) voteColor += "transparent";
+
+    return voteColor;
+  };
+
   return (
     <section className={styles.wrapper}>
       <section
         className={styles.wrapperBg}
         style={{
-          background: `url(${
-            import.meta.env.VITE_BACKDROP_PATH
-          }${backdrop_path})`,
+          background: backdrop_path
+            ? `url(${import.meta.env.VITE_BACKDROP_PATH}${backdrop_path})`
+            : "",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "top",
           backgroundSize: "100% 50%",
@@ -35,7 +48,12 @@ export const MovieDetails = () => {
           <section className={styles.wrapperDetailsCardData}>
             <p className={styles.wrapperDetailsCardDataDesc}>{overview}</p>
             <span>Duração: {runtime} minutos.</span>
-            <span>{vote_average.toFixed(1)}</span>
+            <span
+              className={styles.wrapperDetailsCardDataVote}
+              style={{ backgroundColor: verifyVoteColor(vote_average) }}
+            >
+              {vote_average.toFixed(1)}
+            </span>
           </section>
         </section>
       </section>
