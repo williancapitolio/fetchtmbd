0f61fb3c23707036d40f4f9b8cbbca0ec3ce5d1d
Willian <williansilvap25@gmail.com>
2023-10-16 21:40:50 -0300

    correction loader for rated movies

--- src/pages/RatedMovies/RatedMovies.tsx
+++ src/pages/RatedMovies/RatedMovies.tsx
@@ -1,66 +1,43 @@
-import { useEffect, useState } from "react";
-
-/* import { useLoaderData } from "react-router-dom"; */
-
-import { ratedMoviesLoader } from "../../loaders/ratedMoviesLoader";
+import { useLoaderData } from "react-router-dom";
 
 import { Card } from "../../components/Card";
 
-import { IMovie /* , MoviesType */ } from "../../types/MoviesType";
-
 import styles from "./RatedMovies.module.scss";
 
+import { MoviesType } from "../../types/MoviesType";
+
 export const RatedMovies = () => {
-  const [ratedMoviesList, setRatedMoviesList] = useState<[] | IMovie[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  /* const { movies } = useLoaderData() as MoviesType; */
-
-  const getAllRatedMovies = async () => {
-    const { movies } = await ratedMoviesLoader();
-
-    setRatedMoviesList(movies.results);
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getAllRatedMovies();
-  }, [ratedMoviesList]);
+  const { movies } = useLoaderData() as MoviesType;
 
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.wrapperTitle}>Filmes Avaliados</h1>
-      {isLoading ? (
-        <h2>Buscando...</h2>
-      ) : (
-        <section className={styles.wrapperMovies}>
-          {ratedMoviesList.length > 0 ? (
-            ratedMoviesList
-              .sort((a, b) => b.vote_average - a.vote_average)
-              .map(({ id, poster_path, rating, title, vote_average }) => (
-                <Card
-                  key={id}
-                  id={id}
-                  poster_path={poster_path ? poster_path : ""}
-                  rating={rating}
-                  title={title}
-                  vote_average={vote_average}
-                  ratingAction="delete"
-                />
-              ))
-          ) : (
-            <Card
-              id={0}
-              poster_path={""}
-              rating={0}
-              title={"Nenhum filme avaliado"}
-              vote_average={0}
-              ratingAction=""
-            />
-          )}
-        </section>
-      )}
+      <section className={styles.wrapperMovies}>
+        {movies.results.length > 0 ? (
+          movies.results
+            .sort((a, b) => b.vote_average - a.vote_average)
+            .map(({ id, poster_path, rating, title, vote_average }) => (
+              <Card
+                key={id}
+                id={id}
+                poster_path={poster_path ? poster_path : ""}
+                rating={rating}
+                title={title}
+                vote_average={vote_average}
+                ratingAction="delete"
+              />
+            ))
+        ) : (
+          <Card
+            id={0}
+            poster_path={""}
+            rating={0}
+            title={"Nenhum filme avaliado"}
+            vote_average={0}
+            ratingAction=""
+          />
+        )}
+      </section>
     </section>
   );
 };
