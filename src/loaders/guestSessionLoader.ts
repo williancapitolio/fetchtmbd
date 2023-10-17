6251c0208e613765e43e2d180bae4000b5a8ca53
Willian <williansilvap25@gmail.com>
2023-10-16 22:40:24 -0300

    verify is exist session id on local storage

--- src/loaders/guestSessionLoader.ts
+++ src/loaders/guestSessionLoader.ts
@@ -1,29 +1,29 @@
 import { GuestSessionType } from "../types/GuestSessionType";
 
-export const guestSessionLoader = async (): Promise<GuestSessionType> => {
-  const url: string = import.meta.env.VITE_URL_NEW_GUEST_SESSION;
-  /* const token: string = import.meta.env.VITE_TOKEN; */
+export const guestSessionLoader =
+  async (): Promise<GuestSessionType | null> => {
+    if (localStorage.getItem("guest_session_id")) return null;
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      /* Authorization: token, */
-    },
+    const url: string = import.meta.env.VITE_URL_NEW_GUEST_SESSION;
+    /* const token: string = import.meta.env.VITE_TOKEN; */
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        /* Authorization: token, */
+      },
+    };
+
+    const results = await fetch(url, options);
+
+    if (!results) throw new Error("Something went wrong...");
+
+    const guestSession = await results.json();
+
+    if (!localStorage.getItem("guest_session_id")) {
+      localStorage.setItem("guest_session_id", guestSession.guest_session_id);
+    }
+
+    return { guestSession };
   };
-
-  const results = await fetch(url, options);
-
-  if (!results) throw new Error("Something went wrong...");
-
-  const guestSession = await results.json();
-
-  if (!localStorage.getItem("guest_session_id")) {
-    localStorage.setItem(
-      "guest_session_id",
-      guestSession.guest_session_id
-    );
-  }
-
-  return { guestSession };
-};
